724f7582194bd5f0b7314606d67e2d9c0af54a4a
Hanlin He <hanling.he@gmail.com>
2017-05-20 22:53:23 -0500

    Add commment.

--- leetcode/c/008.c
+++ leetcode/c/008.c
@@ -7,27 +7,44 @@
 
 int _atoi(const char* str, int re, int sig, int flag) {
     if (flag) {
-        /* flag = 1, started. */
+        /* flag = 1, calculation started. */
+
+        /* If other than number, directly return previous result. */
         if (str[0] > '9' || str[0] < '0')
             return re;
+
+        /* If previous result overflow after multiplied by 10, 
+         * return max/min value. */
         if ((re * 10 / 10) != re)
             return sig + 1 ? INT_MAX : INT_MIN;
+
+        /* Otherwise, compute current value. */
         int r = re * 10 + (str[0] - '0') * sig;
+
+        /* If current value overflow, return max/min value. */
         if (r < re && sig == 1)
             return INT_MAX;
         if (r > re  && sig == -1)
             return INT_MIN;
+
+        /* Otherwise, recursively calculate next char. */
         return _atoi(&str[1], r, sig, 1);
     } else {
-        /* flag = 0, not started yet. */
+        /* flag = 0, calculation not started yet. */
+        /* If start with blank, keep going. */
         if (str[0] == ' ')
             return _atoi(&str[1], 0, 1, 0);
+        /* If start with +, start calculation with sig = 1. */
         if (str[0] == '+')
             return _atoi(&str[1], 0, 1, 1);
+        /* If start with -, start calculation with sig = -1. */
         if (str[0] == '-')
             return _atoi(&str[1], 0, -1, 1);
+        /* If start with other characters, return 0. */
         if (str[0] > '9' || str[0] < '0')
             return 0;
+        /* Otherwise, start calculation and recurse on the same char 
+         * with sig = 1. */
         return _atoi(str, 0, 1, 1);
     }
 }
