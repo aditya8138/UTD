a9568cdb955db987e6302d780f663874f7b70cf7
Hanlin He <hanling.he@gmail.com>
2017-05-23 21:40:32 -0500

    Some minor fixed to reduce calculation. Little optimization.

--- leetcode/c/006.c
+++ leetcode/c/006.c
@@ -12,45 +12,32 @@ char* convert(char* s, int numRows) {
     char* r = (char*)malloc((l + 1) * sizeof(char));
     r[l] = '\0';
 
-
-
-    int i = 0;
-    int row = 1;
-    int step1, step2;
+    int i = 0, j, row = 1, step1, step2;
     while (row <= numRows) {
-        //printf("row = %d, i = %d\n", row, i);
-        memmove(&r[i], &s[row - 1], 1);
+        j = row - 1;
+        memmove(&r[i], &s[j], 1);
         i++;
-        //printf("i = %d\n",  i);
 
-        //printf("%s\n", r);
         step1 = 2 * (numRows - row);
         step2 = 2 * (row - 1);
 
-        //printf("step1: %d  step2: %d\n", step1, step2);
-        int j = row - 1;
         while (i < l && j < l) {
-            if (step1 != 0) {
-                if (j + step1 >= l)
-                    break;
+            if (step1) {
                 j += step1;
-                memmove(&r[i], &s[j], 1);
-                //printf("%s\n", r);
-                i++;
-        //printf("i = %d\n",  i);
-            }
-            if (step2 != 0) {
-                if (j + step2 >= l)
+                if (j >= l)
                     break;
-                j += step2;
                 memmove(&r[i], &s[j], 1);
-                //printf("%s\n", r);
                 i++;
-        //printf("i = %d\n",  i);
+            }
+            if (step2) {
+                j += step2;
+                if (j >= l)
+                    break;
+                memmove(&r[i], &s[j], 1);
+                i++;
             }
         }
         row++;
-        //printf("%s\n", r);
     }
 
     return r;
