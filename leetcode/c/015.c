c5e7a184246738f2345d7669bf79a1f13ed5fa8f
Hanlin He <hanling.he@gmail.com>
2017-05-28 17:56:46 -0500

    Merge two functions. Still running slow.

--- leetcode/c/015.c
+++ leetcode/c/015.c
@@ -6,7 +6,7 @@
 #include<stdlib.h>
 
 #define IMAX +100000
-#define DMAX +400000
+#define DMAX +300000
 
 #define print_triple(t) printf("[%d, %d, %d]\n", t[0], t[1], t[2])
 
@@ -14,72 +14,59 @@ int comp (const void * a, const void * b) {
     return *(int *)a - *(int *)b;
 }
 
-int** twoSum(int target, int* nums, int numsSize, int* returnSize) {
-
-    /* First hashtable, mark existence of number in array. */
-    unsigned int h[DMAX] = {0};
-
-    /* Second hashtable, mark triple with same number,
-     * such as: -10 + 5 + 5 = 0. */
-    unsigned int d[DMAX] = {0};
-    int* tr;
-    int** results = (int**)malloc(65535 * sizeof(int*));
-    *returnSize = 0;
-
-    for (int i = 0; i < numsSize; i++) {
-        if (h[-target - nums[i] + IMAX + 1] != 0) {
-            if (h[nums[i] + IMAX + 1] != 0 && 
-                    (-target - nums[i] != nums[i] ||
-                    (-target - nums[i] == nums[i] && 
-                     d[nums[i] + IMAX + 1] != 0)))
-                continue;
-            else {
-                tr = (int*)malloc(3 * sizeof(int));
-                tr[0] = nums[i];
-                tr[1] = -target - nums[i];
-                tr[2] = target;
-                results[(*returnSize)++] = tr;
-                if (-target - nums[i] == nums[i])
-                    d[nums[i] + IMAX + 1] = 1;
-            }
-        }
-        h[nums[i] + IMAX + 1] = i + 1;
-    }
-    return results;
-}
-
 int** threeSum(int* nums, int numsSize, int* returnSize) {
     /* Sort the array before other operation. */
     qsort(nums, numsSize, sizeof (int), comp);
 
-    int** threeSum = (int**)malloc(65535 * sizeof(int*));
+    int** ts = (int**)malloc(18000 * sizeof(int*));
 
-    /* Hashtable used to check duplicate number. */
-    int h[DMAX] = {0};
     /* Traverse the nums array.
      * For each number encountered, if not seen before,
      * get a 3Sum from elements after the number.*/
-    int r, index = 0;
+    int* tr;
+    int r, x = 0, target;
+    int nmax = nums[numsSize - 1];
     *returnSize = 0;
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] > 0)
-            break;
-        if (h[nums[i] + IMAX + 1])
+    for (int i = 0; nums[i] <= 0; i++) {
+        if (i > 0 && nums[i] == nums[i-1])
             continue;
-        int** sts = twoSum(nums[i], &nums[i+1] , numsSize - i - 1, &r);
-        *returnSize += r;
-        for (int j = 0; j < r; j++)
-            threeSum[index++] = sts[j];
-        h[nums[i] + IMAX + 1] = i + 1;
+
+        /* First hashtable, mark existence of number in array. */
+        unsigned int h[DMAX] = {0};
+
+        /* Second hashtable, mark triple with same number,
+            * such as: -10 + 5 + 5 = 0. */
+        unsigned int d[DMAX] = {0};
+        target = -nums[i];
+        for (int j = numsSize - 1; j > i; j--) {
+            x = target - nums[j];
+            if (x > nmax)
+                break;
+            if (h[x + IMAX + 1] != 0) {
+                if (h[nums[j] + IMAX + 1] != 0 &&
+                        (x != nums[j] || (x == nums[j] && d[nums[j] + IMAX + 1] != 0)))
+                    continue;
+                else {
+                    ts[*returnSize] = (int*)malloc(3 * sizeof(int));
+                    ts[*returnSize][0] = nums[j];
+                    ts[*returnSize][1] = x;
+                    ts[*returnSize][2] = nums[i];
+                    *returnSize += 1;
+                    if (x == nums[j])
+                        d[nums[j] + IMAX + 1] = 1;
+                }
+            }
+            h[nums[j] + IMAX + 1] = j + 1;
+        }
     }
-    return threeSum;
+    return ts;
 }
 
 int main() {
-    int n[] = {-1, 0, 1, 2, -1, -4};
+    int n[] = {-1, 0, 1, 2, -1, -4, -5, -9, 3,5,6,7,8,4};
     int size;
 
-    int** ts = threeSum(n, 6, &size);
+    int** ts = threeSum(n, 14, &size);
 
     for (int i = 0; i < size; i++) {
         print_triple(ts[i]);
