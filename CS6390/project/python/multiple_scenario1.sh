0c4004e36084f5c514240b56dbddabd72f9880b4
Hanlin He <hanling.he@gmail.com>
2017-08-10 08:36:48 -0500

    Update test script.

--- CS6390/project/python/multiple_scenario1.sh
+++ CS6390/project/python/multiple_scenario1.sh
@@ -1,10 +1,10 @@
-ssh hxh160630@dc20 ' python3 controller.py "topology1.txt" ' &
-ssh hxh160630@dc10 ' python3 node.py 0 1 "message from 0" 50 ' &
-ssh hxh160630@dc11 ' python3 node.py 1 1 ' &
-ssh hxh160630@dc12 ' python3 node.py 2 2 ' &
-ssh hxh160630@dc13 ' python3 node.py 3 2 "message from 3" 90 ' &
-ssh hxh160630@dc14 ' python3 node.py 4 4 ' & 
-ssh hxh160630@dc16 ' python3 node.py 6 6 ' &
-ssh hxh160630@dc17 ' python3 node.py 7 7 ' &
-ssh hxh160630@dc18 ' python3 node.py 8 8 ' &
-ssh hxh160630@dc19 ' python3 node.py 9 2 "message from 9" 25 ' &
+ssh $1@dc20 ' python3 controller.py "topology1.txt" ' &
+ssh $1@dc10 ' python3 node.py 0 1 "message from 0" 50 ' &
+ssh $1@dc11 ' python3 node.py 1 1 ' &
+ssh $1@dc12 ' python3 node.py 2 2 ' &
+ssh $1@dc13 ' python3 node.py 3 2 "message from 3" 90 ' &
+ssh $1@dc14 ' python3 node.py 4 4 ' & 
+ssh $1@dc16 ' python3 node.py 6 6 ' &
+ssh $1@dc17 ' python3 node.py 7 7 ' &
+ssh $1@dc18 ' python3 node.py 8 8 ' &
+ssh $1@dc19 ' python3 node.py 9 2 "message from 9" 25 ' &
---
+++ CS6390/project/python/remove_old_file.sh
@@ -0,0 +1,8 @@
+# Delete previously generate file.
+rm 0received.txt 1received.txt 2received.txt 3received.txt 4received.txt \
+   5received.txt 6received.txt 7received.txt 8received.txt 9received.txt \
+   from0.txt from1.txt from2.txt from3.txt from4.txt \
+   from5.txt from6.txt from7.txt from8.txt from9.txt \
+   to0.txt to1.txt to2.txt to3.txt to4.txt \
+   to5.txt to6.txt to7.txt to8.txt to9.txt
+
