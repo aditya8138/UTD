6f25fa03a5cfc69126acf860b52393988c2a7bf2
Hanlin He <hanling.he@gmail.com>
2017-08-24 16:02:56 -0500

    Add java solution for 068. Text Justification.

---
+++ leetcode/java/test/L068_TextJustificationSolutionTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class L068_TextJustificationSolutionTest {
+    @Test
+    public void fullJustify() throws Exception {
+        L068_TextJustificationSolution solution = new L068_TextJustificationSolution();
+
+        String[] words = {"This", "is", "an", "example", "of", "text", "justification."};
+        int length = 16;
+        String[] ret = {"This    is    an", "example  of text", "justification.  "};
+        assertArrayEquals(ret, solution.fullJustify(words, 16).toArray());
+        String[] ret1 = {""};
+//        assertArrayEquals(ret2, solution.fullJustify(ret1, 10).toArray());
+        assertArrayEquals(ret1, solution.fullJustify(ret1, 0).toArray());
+//
+        String[] words2 = {"a", "b", "c", "d", "e"};
+        assertArrayEquals(words2, solution.fullJustify(words2, 1).toArray());
+
+        String[] words3 = {"Don't", "go", "around", "saying", "the", "world", "owes", "you", "a", "living;", "the", "world", "owes", "you", "nothing;", "it", "was", "here", "first."};
+        String[] ret3 = {"Don't  go  around  saying  the", "world  owes  you a living; the", "world owes you nothing; it was", "here first.                   "};
+        assertArrayEquals(ret3, solution.fullJustify(words3, 30).toArray());
+    }
+
+    public void fullJustify2() throws Exception {
+        L068_TextJustificationSolution solution = new L068_TextJustificationSolution();
+
+        String[] words = {"This", "is", "an", "example", "of", "text", "justification."};
+        int length = 16;
+        String[] ret = {"This    is    an", "example  of text", "justification.  "};
+        assertArrayEquals(ret, solution.fullJustify2(words, 16).toArray());
+        String[] ret1 = {""};
+//        assertArrayEquals(ret2, solution.fullJustify(ret1, 10).toArray());
+        assertArrayEquals(ret1, solution.fullJustify2(ret1, 0).toArray());
+//
+        String[] words2 = {"a", "b", "c", "d", "e"};
+        assertArrayEquals(words2, solution.fullJustify2(words2, 1).toArray());
+
+        String[] words3 = {"Don't", "go", "around", "saying", "the", "world", "owes", "you", "a", "living;", "the", "world", "owes", "you", "nothing;", "it", "was", "here", "first."};
+        String[] ret3 = {"Don't  go  around  saying  the", "world  owes  you a living; the", "world owes you nothing; it was", "here first.                   "};
+        assertArrayEquals(ret3, solution.fullJustify2(words3, 30).toArray());
+    }
+}
