18c6b4514858aac298ae41eb18d9bcec877c6c6a
Hanlin He <hanling.he@gmail.com>
2017-09-02 09:41:51 -0500

    Add bitwise solution for 078.Subset.

--- leetcode/java/src/L078_Subsets.java
+++ leetcode/java/src/L078_Subsets.java
@@ -23,4 +23,29 @@ public class L078_Subsets {
 
         return ret;
     }
+
+    /**
+     * Bitwise solution. Since a subset can be represented as a bit string of length n, like
+     * '00010000101', and total number of possible subset is 2^n. We can just iterate from 1 to 2^n,
+     * for each number, generate a subset based on the corresponding bit string. A trick to extract
+     * specific bit from a bit string is simply bitwise AND with another specific bit string, for
+     * example '00000001'.
+     *
+     * @param nums : List to be compute.
+     * @return : List of all subset.
+     */
+    public List<List<Integer>> subsetsBitwise(int[] nums) {
+        int size = ((int) Math.pow(2, nums.length));
+        List<List<Integer>> ret = new ArrayList<>(size);
+        List<Integer> subset;
+        for (int i = 0; i < size; i++) {
+            subset = new ArrayList<>();
+            for (int j = i, k = 0; j > 0; j >>= 1, k++) {
+                if ((j & 1) == 1)
+                    subset.add(nums[k]);
+            }
+            ret.add(subset);
+        }
+        return ret;
+    }
 }
--- leetcode/java/test/L078_SubsetsTest.java
+++ leetcode/java/test/L078_SubsetsTest.java
@@ -4,10 +4,21 @@ import static org.junit.Assert.*;
 
 public class L078_SubsetsTest {
     @Test
-    public void subsets() throws Exception {
+    public void subsetsBitwise() throws Exception {
         L078_Subsets solution = new L078_Subsets();
         int[] s = {11,22,33,44,55};
-        System.out.println(solution.subsets(s));
+        System.out.println(solution.subsetsBitwise(s));
+    }
+
+    @Test
+    public void subsetsIntuitive() throws Exception {
+        L078_Subsets solution = new L078_Subsets();
+        int[] s = {11, 22, 33, 44, 55};
+        System.out.println(solution.subsetsIntuitive(s));
+    }
+
+    @Test
+    public void subsets() throws Exception {
     }
 
 }
