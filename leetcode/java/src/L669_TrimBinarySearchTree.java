2749817048a845551c9be79c4978f5a12b858da0
Hanlin He <hanling.he@gmail.com>
2017-09-03 08:43:52 -0500

    Fix wrong problem description.

--- leetcode/java/src/L669_TrimBinarySearchTree.java
+++ leetcode/java/src/L669_TrimBinarySearchTree.java
@@ -1,14 +1,9 @@
 import basic.TreeNode;
 
 /**
- * Given a non-empty special binary tree consisting of nodes with the non-negative value, where each
- * node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this
- * node's value is the smaller value among its two sub-nodes.
- * <p>
- * Given such a binary tree, you need to output the second minimum value in the set made of all the
- * nodes' value in the whole tree.
- * <p>
- * If no such second minimum value exists, output -1 instead.
+ * Given a binary search tree and the lowest and highest boundaries as {@code L} and {@code R},
+ * trim the tree so that all its elements lies in {@code [L, R]} (R>= L). You might need to change
+ * the root of the tree, so the result should return the new root of the trimmed binary search tree.
  */
 public class L669_TrimBinarySearchTree {
 
