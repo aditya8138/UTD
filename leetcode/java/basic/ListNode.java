6880f50accadbac2b276e2bf6ebb2a658fca2a43
Hanlin He <hanling.he@gmail.com>
2017-09-13 21:18:17 -0500

    Add list generator from an array.

--- leetcode/java/basic/ListNode.java
+++ leetcode/java/basic/ListNode.java
@@ -1,5 +1,7 @@
 package basic;
 
+import java.util.ArrayList;
+
 public class ListNode {
 
     public int val;
@@ -7,5 +9,24 @@ public class ListNode {
 
     ListNode(int x) {
         val = x;
+    }
+
+    public static ListNode asList(Integer[] a) {
+        ListNode dummy = new ListNode(0);
+        ListNode cursor = dummy;
+        for (int i : a) {
+            cursor.next = new ListNode(i);
+            cursor = cursor.next;
+        }
+        return dummy.next;
+    }
+
+    public static Integer[] toArray(ListNode head) {
+        ArrayList<Integer> a = new ArrayList();
+        while (head != null) {
+            a.add(head.val);
+            head = head.next;
+        }
+        return a.toArray(new Integer[a.size()]);
     }
 }
