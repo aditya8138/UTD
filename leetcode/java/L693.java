fd09996a4810a99a5e5c103df9fbad9b29258a17
Hanlin He <hanling.he@gmail.com>
2017-10-08 16:41:16 -0500

    Add solution for 693. Binary Number with Alternating Bits.

---
+++ leetcode/java/L693.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * 693. Binary Number with Alternating Bits
+ * <p>
+ * Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits
+ * will always have different values.
+ * <p>
+ * Example 1: Input: 5 Output: True Explanation: The binary representation of 5 is: 101
+ * <p>
+ * Example 2: Input: 7 Output: False Explanation: The binary representation of 7 is: 111.
+ * <p>
+ * Example 3: Input: 11 Output: False Explanation: The binary representation of 11 is: 1011.
+ * <p>
+ * Example 4: Input: 10 Output: True Explanation: The binary representation of 10 is: 1010.
+ */
+public class L693 {
+    @Test
+    public void combine() throws Exception {
+        Solution solution = new Solution();
+
+        assertTrue(solution.hasAlternatingBits(5));
+        assertFalse(solution.hasAlternatingBits(7));
+        assertFalse(solution.hasAlternatingBits(11));
+        assertTrue(solution.hasAlternatingBits(10));
+    }
+
+    class Solution {
+        public boolean hasAlternatingBits(int n) {
+            int ret = ((n >> 1) ^ n);
+            return (ret + 1 & ret) == 0;
+        }
+    }
+}
