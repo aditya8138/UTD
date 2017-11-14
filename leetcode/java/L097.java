12414bc62e1012b7c13b34bb75bf350b242357f3
Hanlin He <hanling.he@gmail.com>
2017-11-13 21:38:39 -0600

    Add DP version directly transformed from recursive solution, time complexity O(n^3). Slow but was able to pass leetcode test. Optimization needed.

--- leetcode/java/L097.java
+++ leetcode/java/L097.java
@@ -9,6 +9,46 @@
  */
 public class L097 {
 
+    class DirectDPSolution {
+        public boolean isInterleave(String s1, String s2, String s3) {
+            int l1 = s1.length();
+            int l2 = s2.length();
+            int l3 = s3.length();
+            boolean[][][] m = new boolean[l3 + 1][l2 + 1][l1 + 1];
+
+            m[l3][l2][l1] = true;
+
+            for (int i3 = l3 - 1; i3 >= 0; i3--) {
+                for (int i2 = l2 - 1; i2 >= 0; i2--) {
+                    m[i3][i2][l1] = (s3.charAt(i3) == s2.charAt(i2) && m[i3 + 1][i2 + 1][l1]);
+                }
+                for (int i1 = l1 - 1; i1 >= 0; i1--) {
+                    m[i3][l2][i1] = (s3.charAt(i3) == s1.charAt(i1) && m[i3 + 1][l2][i1 + 1]);
+                }
+            }
+
+            for (int i3 = l3 - 1; i3 >= 0; i3--) {
+                for (int i2 = l2 - 1; i2 >= 0; i2--) {
+                    for (int i1 = l1 - 1; i1 >= 0; i1--) {
+                        if (s3.charAt(i3) == s1.charAt(i1) && s3.charAt(i3) == s2.charAt(i2))
+                            m[i3][i2][i1] = m[i3 + 1][i2 + 1][i1] || m[i3 + 1][i2][i1 + 1];
+
+                        else if (s3.charAt(i3) == s1.charAt(i1))
+                            m[i3][i2][i1] = m[i3 + 1][i2][i1 + 1];
+
+                        else if (s3.charAt(i3) == s2.charAt(i2))
+                            m[i3][i2][i1] = m[i3 + 1][i2 + 1][i1];
+
+                        else
+                            ;
+                    }
+                }
+            }
+
+            return m[0][0][0];
+        }
+    }
+
     class RecursiveSolution {
         public boolean isInterleave(String s1, String s2, String s3) {
             return isInterleave(s1, s2, s3, 0, 0, 0);
