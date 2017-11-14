1824fd479d94156a01f7d230a651ad884190863f
Hanlin He <hanling.he@gmail.com>
2017-11-13 21:12:51 -0600

    Simplify recursive solution for 097. still too slow, no memoize.

--- leetcode/java/L097.java
+++ leetcode/java/L097.java
@@ -16,11 +16,17 @@ public class L097 {
 
         private boolean isInterleave(String s1, String s2, String s3, int i1, int i2, int i3) {
 
+            if (i1 >= s1.length() && i2 >= s2.length())
+                return i3 >= s3.length();
+
+            if (i3 >= s3.length())
+                return false;
+
             if (i1 >= s1.length())
-                return isSame(s2, s3, i2, i3);
+                return s2.charAt(i2) == s3.charAt(i3) && isInterleave(s1, s2, s3, i1, i2 + 1, i3 + 1);
 
             if (i2 >= s2.length())
-                return isSame(s1, s3, i1, i3);
+                return s1.charAt(i1) == s3.charAt(i3) && isInterleave(s1, s2, s3, i1 + 1, i2, i3 + 1);
 
             final char c1 = s1.charAt(i1);
             final char c2 = s2.charAt(i2);
@@ -37,13 +43,6 @@ public class L097 {
                 return isInterleave(s1, s2, s3, i1, i2 + 1, i3 + 1);
 
             return false;
-        }
-
-        private boolean isSame(String s1, String s2, int i1, int i2) {
-            for (; i1 < s1.length() && i2 < s2.length(); i1++, i2++)
-                if (s1.charAt(i1) != s2.charAt(i2))
-                    return false;
-            return i1 == s1.length() && i2 == s2.length();
         }
     }
 }
